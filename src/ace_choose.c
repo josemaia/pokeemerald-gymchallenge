e55b736cb4aeee23c38c29df005edc7fa7d0308f
José Maia <jose.maia@ctw.bmwgroup.com>
2025-05-19 16:49:21 +0100

    Setup ace selection

--- src/ace_choose.c
+++ src/ace_choose.c
@@ -473,7 +473,7 @@ static void Task_AceChoose(u8 taskId)
 {
     CreateAcePokemonLabel(gTasks[taskId].tAceSelection);
     DrawStdFrameWithCustomTileAndPalette(0, FALSE, 0x2A8, 0xD);
-    AddTextPrinterParameterized(0, FONT_NORMAL, gText_BirchInTrouble, 0, 1, 0, NULL);
+    AddTextPrinterParameterized(0, FONT_NORMAL, gText_AceSelection, 0, 1, 0, NULL);
     PutWindowTilemap(0);
     ScheduleBgCopyTilemapToVram(0);
     gTasks[taskId].func = Task_HandleAceChooseInput;
--- src/strings.c
+++ src/strings.c
@@ -125,6 +125,7 @@ const u8 gText_IdNumberSlash[] = _("IDNo. /"); // Unused
 const u8 gText_Name[] = _("NAME");
 const u8 gText_IDNumber[] = _("IDNo.");
 const u8 gText_BirchInTrouble[] = _("PROF. BIRCH is in trouble!\nRelease a POKéMON and rescue him!");
+const u8 gText_AceSelection[] = _("Select your ace POKéMON.\nIts type will be used for your gym.");
 const u8 gText_ConfirmStarterChoice[] = _("Do you choose this POKéMON?");
 const u8 gText_ConfirmAceChoice[] = _("Do you choose this POKéMON,\n and its type for your gym?");
 const u8 gText_Pokemon4[] = _("POKéMON"); // Unused
